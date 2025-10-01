19cf0bd795a729dd9e189567880a45e755f6165d
hrzndelta <hrzndelta@proton.me>
2025-10-01 17:26:32 +0800

    DeltaAddons: Restructure project (again, this time for modularity)

--- config.mk
+++ config.mk
@@ -1,4 +1,9 @@
 ## Modules
 ifeq ($(TARGET_SHIP_JAMESDSP),true)
+include vendor/DeltaAddons/config/jamesdsp.mk
 endif
-
+ifeq ($(WITH_MICROG),true)
+include vendor/DeltaAddons/config/microg.mk
+endif
+include vendor/DeltaAddons/config/media.mk
+include vendor/DeltaAddons/config/packages.mk
